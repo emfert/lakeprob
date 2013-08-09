f456733b6648319a90bdff5fd6362b52b2c232ee
Emily Fertig <efertig@mit.edu>
2013-08-09 15:46:58 -0400

    Working optimization model for Lemperts version of the lake problem with only one time step size

--- Lempert_LakeProbOpt.m
+++ Lempert_LakeProbOpt.m
@@ -1,17 +1,20 @@
 % % This reproduces the lake problem model in Lempert and Collins (2007)
 
-% function [Sopt, deltaLopt, L0opt] = Lempert_LakeProbOpt()
-%     [Sopt, deltaLopt, L0opt] = [1,2,3];
-% end
+function [x,fval,exitflag] = Lempert_LakeProbOpt()
+    x0 = [2.5, .3, .3];
+    options = optimset('Display','iter','TolFun',1e-3,'TolX',1e-3,'MaxIter',100);
+    [x,fval,exitflag] = fminsearch(@LCValFcn,x0,options);
+end
 
-% function V = LCValFcn(S, deltaL, L0)
-clear
-S = 2.8;
-deltaL = .27;
-L0 = .31;
+function V = LCValFcn(x)
+% clear
+% S = 2.8;
+% deltaL = .27;
+% L0 = .31;
+S = x(1);
+deltaL = x(2);
+L0 = x(3);
 
-
-Pcrit = [.2 .7];
 B = .2;              % decay rate of P concentration (B in lempert paper)
 b = .1;                 % natural baseline loading
 r = .25;                % P recycling parameter
@@ -25,15 +28,7 @@ lmbda = .1;             % distance from xcrit req for learning
 q = 2;                  % noise exponent
 
 N = 2000;             % no. samples total, for initial data collection
-
-NPt = 20;%41;               % no. grid points for Pt (concentration)
-Npii = 20;%41;              % no. grid points for pii (probabilities)
-Nlt = 20;%161;              % no. grid points for P loadings
-
-Pt = linspace(0,1,NPt);
-pii = linspace(0,1,Npii);
-lt = linspace(0,.8,Nlt);
-T = 1000;                 % time span
+T = 100;                 % time span
 
 Xcrit = [.3 .4 .5 .6 .7 .8 .9];
 PXcrit = [0 0 0 .05 .25 .45 .25];
@@ -60,6 +55,7 @@ for i = 1:length(Xcrit)
     U(:,i,1) = alphaa*L0 - bbeta*(X0>Xcrit(i));
 end
 
+rng(1) % seed RNG
 % do Monte Carlo sim
 
 for i = 1:N % for each model
@@ -70,7 +66,7 @@ for i = 1:N % for each model
             bt = randn*omga + b;
             Xt(i,j,k) = B*Xt(i,j,k-1) + bt + L(i,j,k-1) + r*(Xt(i,j,k-1)>=Xcrit(j));
             
-            gmmat = gmma*(Xt(i,j,k)>=Xcrit(j)) + gmma*exp(((Xcrit(j) - Xt(i,j,k))/lmbda)^2)*(Xt(i,j,k)<Xcrit(j));
+            gmmat = gmma*(Xt(i,j,k)>=Xcrit(j)) + gmma*exp(((Xcrit(j) - Xt(i,j,k))/lmbda)^q)*(Xt(i,j,k)<Xcrit(j));
             nu = nu_old*gmmat/(nu_old+gmmat);
             Zt = randn*gmma+Xcrit(j);
             Xcrit_est(i,j,k) = Xcrit_est(i,j,k-1) + (nu/(nu+gmmat))*(Zt - Xcrit_est(i,j,k-1));
@@ -93,25 +89,25 @@ for i = 1:length(Xcrit)
 end
 
 Vp2 = mean(Vp,1);
-V = Vp2*PXcrit';
+V = -Vp2*PXcrit'; % made this negative because we're maximizing
 
-meanL = squeeze(mean(L,1));
-meanXcrit_est = squeeze(mean(Xcrit_est,1));
-meanXt = squeeze(mean(Xt,1));
+%V.meanL = squeeze(mean(L,1));
+%V.meanXcrit_est = squeeze(mean(Xcrit_est,1));
+%V.meanXt = squeeze(mean(Xt,1));
 
-% end
+end
 
-%% test figures
-
-figure
-ind = 7;
-plot((0:T-1)/1,meanL(ind,:))
-ylim([0 1])
-xlim([0 100])
-hold on
-plot((0:T-1)/1,meanXcrit_est(ind,:))
-plot((0:T-1)/1,meanXt(ind,:))
-grid on
+% %% test figures
+% 
+% figure
+% ind = 7;
+% plot((0:T-1)/1,meanL(ind,:))
+% ylim([0 1])
+% xlim([0 100])
+% hold on
+% plot((0:T-1)/1,meanXcrit_est(ind,:))
+% plot((0:T-1)/1,meanXt(ind,:))
+% grid on
 
             
             
